441562c546863da48d040c0d5efd53193e58ee80
danielevergani <dvergani94@gmail.com>
2021-03-08 16:13:40 +0100

    ultimato es pari dispari

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -1,1 +1,26 @@
-alert("pari dispari");
+// inizializzo variabili
+
+var scelta = prompt("pari o dispari ?");
+var numero;
+do {
+    numero = parseInt( prompt("scrivi un numero tra 1 e 5" ) );
+} while ((numero < 1) || (numero > 5));
+
+function pariOdispari(previsione, num){
+    
+    var numRandom = Math.floor(Math.random() * 5) + 1;
+    var totale = numRandom + num;
+    
+    // condizione da verificare
+
+    if ( ( (totale % 2 == 0) && previsione == "pari" ) || ( (totale % 2 != 0) && previsione == "dispari" ) ){
+        return "hai vinto con il numero " + totale
+    }
+    else{
+        return "hai perso con il numero " + totale
+    }
+}
+
+
+var risultato = pariOdispari(scelta, numero);
+console.log(risultato);
