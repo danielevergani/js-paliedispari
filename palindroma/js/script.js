c35ec8654eae177a96b181d44b52026285351c6c
danielevergani <dvergani94@gmail.com>
2021-03-08 15:10:29 +0100

    terminate condizioni essenziali x palindromo

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,1 +1,19 @@
-alert("palindroma");
+var parola = prompt("inserisci una parola, ti dirò se è una parola palindroma");
+// creo funzione
+
+function palindroma(x){
+    // inizialirro array vuoto
+    var lista = [];
+
+    // inserisco parola in array
+    for ( var i=0; i<x.length; i++){
+        lista.push(x[i]);
+    } 
+    console.log(lista);
+
+    // inverto array e assegno a un'altra variabile
+    var revLista = lista.reverse();
+    console.log(revLista);
+}
+
+palindroma(parola);
